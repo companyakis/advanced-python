5ee42845af4b8ad204ae78a5d3bfe0fa69209c80
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-18 13:14:53 +0300

    Update 0100-oop-003-encapsulation-getters2.py

--- 0100-oop-003-encapsulation-getters2.py
+++ 0100-oop-003-encapsulation-getters2.py
@@ -1,8 +1,12 @@
 class BankAccountInfo:
-    def __init__(self, account_holder: str, account_number: int, account_balance: float = 0.0):
+    def __init__(self, account_holder: str, account_number: str, account_balance: float = 0.0):
         self._account_holder = account_holder
         self._account_number = account_number
         self._account_balance = account_balance
+        
+    # getter methods
+    # properties are NOT public now
+    # we need getter methods
     
     def get_account_holder(self):
         return self._account_holder
@@ -19,7 +23,7 @@ if __name__ == "__main__":
     
     customer_bilge = BankAccountInfo(
         account_holder="Bilge Kağan",
-        account_number=1000001,
+        account_number="1000001",
         account_balance=8759684.42
     )
     

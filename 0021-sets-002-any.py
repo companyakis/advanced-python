630aa2d43574ae4753979fd4320e7feff4e7f1e0
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-20 15:28:15 +0300

    Create 0021-sets-002-any.py

---
+++ 0021-sets-002-any.py
@@ -0,0 +1,15 @@
+years = [2018, 2020, 2022, 2023, 2024, 2018, 1990, 2013, 2016, 1987, 1923, 2012, 2024]
+
+years_set_from_list = set(years)
+
+search_years = {1990, 2023, 2016}
+
+result = any(year in years_set_from_list for year in search_years)
+
+print(result) # True
+
+if result:
+    print("Yes, we have some data")
+    
+else:
+    print("No data!")

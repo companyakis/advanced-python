565ec572895d7dda406d2ea7adbe74f7e5ef9c21
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-20 15:23:01 +0300

    Create 0021-sets-001-intro.py

---
+++ 0021-sets-001-intro.py
@@ -0,0 +1,19 @@
+names = {"Mustafa", "Aygün", "Kağan", "Hakan", "Hakan"}
+
+print(names) # Only one "Hakan" => {'Mustafa', 'Kağan', 'Aygün', 'Hakan'}
+
+years = [2018, 2020, 2022, 2023, 2024, 2018]
+
+set_from_list = set(years)
+
+print(set_from_list) # {2018, 2020, 2022, 2023, 2024}
+
+# if else
+
+if 2017 in set_from_list:
+    print("Year 2017 is in te set")
+       
+else:
+    print("Not in the set!")
+    
+    

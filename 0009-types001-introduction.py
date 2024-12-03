d90ba0d2dd4062b9575e4b95c96a492331c5d558
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-12-03 23:22:45 +0300

    Create 0009-types001-introduction.py

---
+++ 0009-types001-introduction.py
@@ -0,0 +1,9 @@
+def proverb_conversion(proverb: str) -> str:
+    
+    return proverb.title()
+
+proverb1 = "a rolling stone gathers no moss!"
+
+result = proverb_conversion(proverb1)
+
+print(result) # A Rolling Stone Gathers No Moss!

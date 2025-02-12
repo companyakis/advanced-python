9d81c0c9ed64a365abc8e273e131e3a8bfd3bad8
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-12 21:25:57 +0300

    Create 0023-lambda1.py

---
+++ 0023-lambda1.py
@@ -0,0 +1,13 @@
+summation = lambda x, y, z: x + y + z
+
+print(summation(4, 6, 10)) # 20
+
+sum_of_squares = lambda x, y: x ** 2 + y ** 2
+
+print(sum_of_squares(3, 4)) # 25
+
+squared_root = lambda x: x ** 0.5
+
+print(squared_root(2)) # 1.4142135623730951
+
+print(squared_root(4)) # 2.0

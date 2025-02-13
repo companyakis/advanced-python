594dc438874334d3b45e154729c3c719011ff15c
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-13 19:18:11 +0300

    Create 0024-map2.py

---
+++ 0024-map2.py
@@ -0,0 +1,12 @@
+years = ["2023", "2024", "2025"]
+
+int_years = list(map(int, years))
+
+print(int_years) # [2023, 2024, 2025]
+
+names = ["mustafa", "aygün", "haKan", "AYHAN"]
+
+names2 = list(map(str.title, names))
+
+print(names2) # ['Mustafa', 'Aygün', 'Hakan', 'Ayhan']
+
